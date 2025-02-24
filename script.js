e6ec0ad3f373859e9a7715de8147808b2f13a2dc
lizlux <liz@lizlux.net>
2025-02-24 11:18:45 -0800

    minor cleanup

--- script.js
+++ script.js
@@ -4,18 +4,20 @@ import testLines from "./modules/tests.js";
 import getLines from "./modules/getLines.js";
 import createBoard from "./modules/createBoard.js";
 
-const handleError = () => {
-  document.querySelector(".error-message").classList.remove("hidden");
-};
-
-// Execute the code
 try {
   const allLines = getLines();
   createBoard(allLines);
-
-  // Uncomment next line to run tests
-  // testLines(allLines);
 } catch (e) {
   console.log(e);
-  handleError();
+  document.querySelector(".error-message").classList.remove("hidden");
 }
+
+// TODO: move test execution elsewhere
+const runTests = () => {
+  const allLines = getLines();
+  createBoard(allLines);
+  testLines(allLines);
+};
+
+// Uncomment next line to run tests
+// runTests();
